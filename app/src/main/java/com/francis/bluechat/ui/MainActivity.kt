7202c229e7fb92cdb0b861cb581d48c09999767d
Francis Akpan <francisf.akpan@gmail.com>
2021-12-05 22:13:19 +0100

    Discover nearby devices

--- app/src/main/java/com/francis/bluechat/ui/MainActivity.kt
+++ app/src/main/java/com/francis/bluechat/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.francis.bluechat.ui
 
+import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothClass
 import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
@@ -14,6 +19,7 @@ import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.francis.bluechat.R
@@ -24,6 +30,7 @@ import com.francis.bluechat.ui.adapter.DeviceAdapter
 import com.francis.bluechat.ui.dagger.MainComponent
 import com.francis.bluechat.utils.SnackBarUtil
 import com.francis.bluechat.utils.missingSystemFeature
+import com.google.android.material.switchmaterial.SwitchMaterial
 import java.lang.reflect.Method
 import javax.inject.Inject
 import javax.inject.Provider
@@ -31,8 +38,9 @@ import javax.inject.Provider
 class MainActivity : AppCompatActivity(), Provider<MainComponent> {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var switch: SwitchMaterial
 
-    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -53,6 +61,7 @@ class MainActivity : AppCompatActivity(), Provider<MainComponent> {
             if (result.resultCode == Activity.RESULT_OK) {
                 queryPairedDevices()
             } else {
+                switch.isChecked = false
                 SnackBarUtil.instance?.showSnackBar(this, "Error turning on bluetooth")
             }
         }
@@ -61,13 +70,62 @@ class MainActivity : AppCompatActivity(), Provider<MainComponent> {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             when (result.resultCode) {
                 DURATION -> {
-                    TODO("")
+//                    TODO("")
                 }
                 Activity.RESULT_CANCELED -> {
-                    TODO("")
+//                    TODO("")
                 }
             }
         }
+
+    private val locationLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+            if (result.values.all { it == true }) {
+                bluetoothAdapter.startDiscovery().also { isOn ->
+                    binding.swipeLayout.isRefreshing = isOn
+                }
+            } else {
+                SnackBarUtil.instance?.showSnackBar(
+                    this,
+                    "Turn on location to discover bluetooth devices around you"
+                ) {
+                    checkLocationPermission()
+                }
+            }
+        }
+
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+
+                    if (device?.let { isPhone(it) } == true) {
+                        val name = device.name ?: return
+                        val address = device.address ?: return
+
+                        with(adapter) {
+                            val devices = currentList.toMutableSet().apply {
+                                add(Device(name, address))
+                            }
+                            submitList(devices.toList())
+                        }
+                    }
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+                    binding.swipeLayout.isRefreshing = true
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                    binding.swipeLayout.isRefreshing = false
+                }
+            }
+        }
+
+    }
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,15 +134,92 @@ class MainActivity : AppCompatActivity(), Provider<MainComponent> {
 
         get().inject(this)
 
-        checkBluetoothFeature()
-        checkToEnable()
-
         binding.recyclerView.adapter = adapter
+
+        checkBluetoothFeature()
+        registerFoundReceiver()
+        onSwipeRefreshLayoutInflated()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
+    private fun registerFoundReceiver() {
+        // Register for broadcasts when a device is discovered.
+        IntentFilter().let { filter ->
+            filter.addAction(BluetoothDevice.ACTION_FOUND)
+            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+            registerReceiver(receiver, filter)
+        }
+    }
+
+    private fun onSwipeRefreshLayoutInflated() {
+        with(binding.swipeLayout) {
+            setOnRefreshListener {
+                if (isRefreshing) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        checkLocationPermission()
+                    }
+
+                    if (bluetoothAdapter.isDiscovering) bluetoothAdapter.cancelDiscovery()
+
+                    bluetoothAdapter.startDiscovery().also { isOn ->
+                        isRefreshing = isOn
+                    }
+                } else if (!bluetoothAdapter.isEnabled) {
+                    isRefreshing = false
+                    SnackBarUtil.instance?.showSnackBar(
+                        this@MainActivity,
+                        "Bluetooth is not turned on"
+                    ) {
+                        turnOn()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLocationPermission() {
+        (getSystemService(Context.LOCATION_SERVICE) as LocationManager).also {
+            if (!it.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                startActivity(Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            locationLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+        setSwitchListener(menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setSwitchListener(menu: Menu?) {
+        val menuItem = menu!!.findItem(R.id.power)
+        switch = menuItem.actionView as SwitchMaterial
+        switch.isChecked = bluetoothAdapter.isEnabled
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) checkToEnable()
+            else bluetoothAdapter.disable()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -116,12 +251,15 @@ class MainActivity : AppCompatActivity(), Provider<MainComponent> {
         }
     }
 
+    private fun turnOn() {
+        Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE).let { intent ->
+            bluetoothLauncher.launch(intent)
+        }
+    }
+
     private fun checkToEnable() {
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (!bluetoothAdapter.isEnabled) {
-            Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE).let { intent ->
-                bluetoothLauncher.launch(intent)
-            }
+            turnOn()
         } else if (bluetoothAdapter.isEnabled) {
             queryPairedDevices()
         }
--- app/src/main/java/com/francis/bluechat/utils/SnackBarUtil.kt
+++ app/src/main/java/com/francis/bluechat/utils/SnackBarUtil.kt
@@ -2,7 +2,9 @@ package com.francis.bluechat.utils
 
 import android.app.Activity
 import android.graphics.Color
+import android.view.View
 import android.widget.TextView
+import com.francis.bluechat.R
 import com.google.android.material.snackbar.Snackbar
 
 class SnackBarUtil {
@@ -21,6 +23,21 @@ class SnackBarUtil {
         val sv = snackBar?.view
         val st = sv?.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
         st?.setTextColor(Color.WHITE)
+        snackBar?.show()
+    }
+
+    fun showSnackBar(activity: Activity, message: String?, action: (View) -> Unit) {
+        snackBar = Snackbar.make(
+            activity.findViewById(android.R.id.content),
+            message.toString(),
+            Snackbar.LENGTH_LONG
+        )
+
+        snackBar!!.setAction(activity.getString(R.string.snackbar_action_text), action)
+
+        val sv = snackBar!!.view
+        val st = sv.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+        st.setTextColor(Color.WHITE)
         snackBar?.show()
     }
 
